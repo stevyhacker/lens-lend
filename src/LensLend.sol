fdbdf9da6c83a81f839a46e4d2dcb5b9929b1e97
Stevan Bogosavljevic <stevyhacker@gmail.com>
2023-11-19 13:35:44 +0100

    Add updateDebt logic

--- src/LensLend.sol
+++ src/LensLend.sol
@@ -5,38 +5,55 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+/// @title LensLend
+/// @notice Lend USDC against Lens profile NFTs
 contract LensLend {
 
     struct LensBorrower {
-        uint256 lensProfileTokenId;
+        uint256 profileId;
         uint256 reputation;
         uint256 debt;
+        uint256 lastUpdated;
     }
 
     struct UsdcLender {
         address lender;
         uint256 suppliedBalance;
+        uint256 lastUpdated;
     }
 
-    mapping(uint256 profileId => LensBorrower) private lensBorrowers;
-    mapping(address user => UsdcLender) private lenders;
+    mapping(uint256 profileId => LensBorrower) public lensBorrowers;
+    mapping(address user => UsdcLender) public lenders;
 
-    // Constants
-    IERC20 public immutable usdc; // USDC on Polygon 0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359
-    IERC721 public immutable lensProfileCollection; // Lens profile collection on Polygon 0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d
+    uint256 public yearlyInterestRate = 1e17; // 10% per year
+
+    IERC20 public immutable usdc;
+    IERC721 public immutable lensProfileCollection;
 
     constructor(address _usdcAddress, address _lensProfileCollection) {
         usdc = IERC20(_usdcAddress);
         lensProfileCollection = IERC721(_lensProfileCollection);
     }
 
-    function addNewBorrower(uint256 _lensProfileTokenId) external {
+    /// @notice Adds a Lens profile to the lending pool
+    function addBorrower(uint256 _profileId) external {
+        require(lensProfileCollection.ownerOf(_profileId) == msg.sender, "Only the owner can add a Lens profile");
+        require(lensBorrowers[_profileId].profileId == 0, "Lens profile already added");
+
         LensBorrower memory newBorrower;
+        newBorrower.profileId = _profileId;
+        newBorrower.reputation = getReputation(_profileId);
+        lensBorrowers[_profileId] = newBorrower;
 
-        newBorrower.reputation = getReputation(_lensProfileTokenId);
-        newBorrower.lensProfileTokenId = _lensProfileTokenId;
+        lensProfileCollection.transferFrom(msg.sender, address(this), _profileId);
+    }
 
-        lensBorrowers[_lensProfileTokenId] = newBorrower;
+    /// @notice Removes a Lens profile from the lending pool
+    function removeBorrower(uint256 _profileId) external {
+        require(lensProfileCollection.ownerOf(_profileId) == msg.sender, "Only the owner can remove a Lens profile");
+        require(lensBorrowers[_profileId].profileId != 0, "Lens profile not added");
+
+        delete lensBorrowers[_profileId];
     }
 
     // Function to borrow USDC against Lens profile NFT
@@ -52,7 +69,7 @@ contract LensLend {
         usdc.transferFrom(msg.sender, address(this), _amount);
     }
 
-    /// @dev Repays borrowed USDC debt of a lens profile borrower
+    /// @notice Repays borrowed USDC debt of a lens profile borrower
     function repayDebt(uint256 _tokenId, uint256 _amount) external {
         LensBorrower storage borrower = lensBorrowers[_tokenId];
         require(borrower.debt >= _amount, "Exceeds borrowed balance");
@@ -60,7 +77,7 @@ contract LensLend {
         usdc.transfer(msg.sender, _amount);
     }
 
-    /// @dev Calculate the reputation of a Lens profile holder
+    /// @notice Calculate the reputation of a Lens profile holder
     function getReputation(uint256 _tokenId) public view returns (uint256) {
         //todo: calculate reputation using lens protocol or phi land score or gitcoin passport
 
@@ -72,7 +89,7 @@ contract LensLend {
         return lensBorrowers[_tokenId].debt;
     }
 
-    /// @dev Lenders can supply USDC to the lending pool
+    /// @notice Lenders can supply USDC to the lending pool
     function supply(uint256 _amount) external {
         require(usdc.balanceOf(msg.sender) >= _amount, "Insufficient USDC balance");
 
@@ -84,23 +101,43 @@ contract LensLend {
         usdc.transferFrom(msg.sender, address(this), _amount);
     }
 
-    /// @dev Lenders can withdraw their USDC from the lending pool
+    /// @notice Lenders can withdraw their USDC from the lending pool
     function withdraw(uint256 _amount) external {
         require(lenders[msg.sender].suppliedBalance >= _amount, "Insufficient supplied balance");
 
         UsdcLender storage lender = lenders[msg.sender];
+
         lender.suppliedBalance -= _amount;
         lenders[msg.sender] = lender;
 
         usdc.transfer(msg.sender, _amount);
     }
 
+    /// @notice Update debt of the borrowers with the current interest rate
+    /// @dev This function can be called every 24h
+    function updateDebt(uint256 _profileId) internal {
+        LensBorrower storage borrower = lensBorrowers[_profileId];
+
+        uint256 timeSinceLastUpdate = block.timestamp - borrower.lastUpdated;
+        require(timeSinceLastUpdate < 24 hours, "Cannot update debt more than once per day");
+
+        uint256 currentDebt = borrower.debt;
+        uint256 interestRate = yearlyInterestRate * (timeSinceLastUpdate / 365 days);
+        uint256 accruedInterest = currentDebt * interestRate;
+
+        require(usdc.balanceOf(address(this)) >= accruedInterest, "Insufficient USDC balance in the lending pool");
+        borrower.lastUpdated = block.timestamp;
+        borrower.debt += accruedInterest;
+    }
+
+
     /// @dev Liquidate a Lens profile borrower if their debt is too high
-    function liquidate(uint256 _tokenId) external {
-        LensBorrower storage borrower = lensBorrowers[_tokenId];
+    function liquidate(uint256 _profileId) external {
+        LensBorrower storage borrower = lensBorrowers[_profileId];
         require(borrower.debt > 0, "Lens profile has no debt");
 
         //todo: take their NFT and sell it on the market to repay the debt
+
     }
 
 }
---
+++ test/LensLend.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.21 <0.9.0;
+
+import { PRBTest } from "@prb/test/PRBTest.sol";
+import { console2 } from "forge-std/console2.sol";
+import { StdCheats } from "forge-std/StdCheats.sol";
+
+import { LensLend } from "../src/LensLend.sol";
+
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+}
+
+
+contract LensLendTest is PRBTest, StdCheats {
+    LensLend internal lensLend;
+
+    /// @dev A function invoked before each test case is run.
+    function setUp() public virtual {
+        // Instantiate the contract-under-test.
+        lensLend = new LensLend(
+            address(0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359), // USDC on Polygon
+            address(0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d) // Lens Profile Collection on Polygon
+        );
+
+        string memory alchemyApiKey = vm.envOr("API_KEY_ALCHEMY", string(""));
+        if (bytes(alchemyApiKey).length == 0) {
+            revert("API_KEY_ALCHEMY not set");
+        }
+
+        //run the test against the mainnet fork.
+        vm.createSelectFork({ urlOrAlias: "polygon", blockNumber: 50_130_288 });
+    }
+
+    /// @dev Fork test that runs against an Ethereum Mainnet fork. For this to work, you need to set `API_KEY_ALCHEMY`
+    /// in your environment You can get an API key for free at https://alchemy.com.
+    function testFork_Example() external {
+
+        address expectedUsdc = 0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359;
+        address expectedLensProfile = 0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d;
+
+        console2.log("lensLend address is %s", address(lensLend));
+
+        address actualLensProfile = address(lensLend.lensProfileCollection());
+        address actualUsdc = address(lensLend.usdc());
+//        uint256 actualBalance = IERC20(usdc).balanceOf(holder);
+
+        assertEq(actualUsdc, expectedUsdc);
+        assertEq(actualLensProfile, expectedLensProfile);
+    }
+}
